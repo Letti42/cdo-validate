d522b9969c753538f8d99500682ed8d5c0e0bcd2
Letti42 <letti42@outlook.com>
2022-01-15 16:07:06 -0800

    Update
    Send objects instead of normal strings

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const fetch = require('cross-fetch');
 
 app.get('/',(req,res)=>{
   if(req.query.url === undefined){
-    res.send('Bad request');
+    res.send({"err":"Bad Request","type":400});
     return;
   }
   var urlRedir = req.query.url;
   if(urlRedir !== undefined)urlRedir = decodeURIComponent(urlRedir).toLowerCase().includes('://')?'http://'+decodeURIComponent(urlRedir).split('://')[1]:"http://"+decodeURIComponent(urlRedir);
   fetch(decodeURIComponent(encodeURIComponent(urlRedir))).then((response) => response.text()).then((text)=>{
-    res.send('good');
+    res.send({"good":"true","type":200});
   })
   .catch((err)=>{
-    res.send('err');
+    res.send({"good":"false","type":404});
   })
 });
 
